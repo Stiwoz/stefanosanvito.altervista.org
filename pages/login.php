126394837fadeb6f7b2941b4d6371f2467ffe8c5
Stefano <stiwou@gmail.com>
2016-01-19 18:50:54 +0100

    Implemented Bootstrap and redid navbar and footer
    Implemented Bootstrap
    Added a fully customized *.css version of Bootstrap
    Added *.less variables file
    Removed redundant code in style.css
    Updated all pages with the new navbar and footer
    Edited scrollbars

--- pages/login.php
+++ pages/login.php
@@ -6,27 +6,40 @@
     <meta name="viewport" content="width=device-width, initial-scale=1"/>
     <meta name="author" content="Sanvito Stefano"/>
     <meta name="description" content="School Finals Website Project"/>
-    
-    <link rel="stylesheet" href="../bootstrap-3.3.6-dist/css/bootstrap.min.css"/>
-    <link rel="stylesheet" href="../css/style.css"/>
+
+    <link rel="stylesheet" href="../css/custom-theme-bootstrap.css"/>
     <link rel="stylesheet" href="../css/font-awesome-4.4.0/css/font-awesome.css"/>
     <link rel="stylesheet" type="text/css" href="../js/sweetalert2-master/dist/sweetalert2.css"/>
-    
+    <link rel="stylesheet" href="../css/style.css"/>
+
     <title>Sanvito Stefano's Website</title>
 </head>
 <body>
-    <!--- --- --- Header --- --- --->
-    <div class="navbar" id="home">
-        <a href="../index.html"><img src="../img/logo.png" class="logo"/></a>
-        <ul class="navbar-ul">
-            <a class="navbar-a" href="../index.html"><li class="navbar-li"><i class="fa fa-home fa-fw"></i> Home</li></a>
-            <a class="navbar-a" href="tesina.html"><li class="navbar-li"><i class="fa fa-graduation-cap fa-fw"></i> Tesina</li></a>
-            <a class="navbar-a" href="esame.html"><li class="navbar-li"><i class="fa fa-tv fa-fw"></i> Esame</li></a>
-            <a class="navbar-a" href="login.php"><li class="navbar-li thispage"><i class="fa fa-cogs fa-fw"></i> Login</li></a>
-        </ul>
-    </div>
+    <!---  Header  --->
+    <nav class="navbar navbar-default navbar-fixed-top">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#buttons" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="../index.html"><img class="navbar-brand" alt="Level Up!" src="../img/logo.png"/></a>
+            </div>
+            <div class="collapse navbar-collapse" id="#buttons">
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="navbar-li"><a class="navbar-a" href="../index.html"><i class="fa fa-home fa-fw"></i> Home</a></li>
+                    <li class="navbar-li"><a class="navbar-a" href="../pages/tesina.html"><i class="fa fa-graduation-cap fa-fw"></i> Tesina</a></li>
+                    <li class="navbar-li"><a class="navbar-a" href="../pages/esame.html"><i class="fa fa-tv fa-fw"></i> Esame</a></li>
+                    <li class="navbar-li active"><a class="navbar-a" href="../pages/login.php"><i class="fa fa-cogs fa-fw"></i> Login</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
     <!--- --- --- Content --- --- --->
-    <div class="container">
+    <div class="container-fluid">
         <div class="div-left">
             <!--<iframe id="inputFrame" src="../pages/inputForm.html" id="inputForm"></iframe>-->
             <form id="regForm" name="regForm" method="post" action="sendform.php">
@@ -78,19 +91,19 @@
                     $password = "";
                     $dbname = "my_stefanosanvito";
                     $dbtable ="logged_users";
-                
+
                     $sql = "SELECT * FROM ".$dbtable." ORDER BY ID";
 
                     $conn = mysqli_connect($servername, $username, $password, $dbname);
                     if (!$conn)
                         die("Can't connect to database");
-                    
+
                     $result = mysqli_query($conn, $sql);
                     if (!$result)
                         die("Query to show fields from table failed");
 
                     $fields_num = mysqli_num_fields($result);
-                
+
                 /* --- --- Header --- --- */
                     echo "<tr>";
                     for($i=0; $i<($fields_num+2); $i++) {
@@ -98,14 +111,14 @@
                         echo "<th>{$field->name}</th>";
                     }
                     echo "</tr>";
-                
+
                 /* --- --- Content --- --- */
                     while($row = mysqli_fetch_row($result)) {
                         echo "<tr>";
 
                         foreach($row as $cell)      // $row is array... foreach( .. ) puts every element of $row to $cell variable
                             echo "<td>$cell</td>";
-                        
+
                         echo "<td><a href='#' class='table-logged-icon' onclick=\"editRow(".$row[0].");\"><img src=\"../img/edit.png\" alt=\"Modifica\" /></a></td><td><a href='#' class='table-logged-icon' onclick=\"deleteRow(".$row[0].");\"><img src=\"../img/delete.png\" alt=\"Elimina\" /></a></td>";
                         echo "</tr>";
                     }
@@ -114,24 +127,30 @@
             </table>
         </div>
     </div>
-    <!--- --- --- Footer --- --- --->
+
+    <!---  Footer  --->
     <div class="footer">
-        <table class="footer-table">
-            <tr>
-                <td><div class="info">Stefano Sanvito<br>s.stefano.sanvito@gmail.com</div></td>
-                <td>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-2">
+                    <div class="info">Stefano Sanvito<br>s.stefano.sanvito@gmail.com</div>
+                </div>
+                <div class="col-md-3"></div>
+                <div class="col-md-6">
                     <ul class="footer-ul">
                         <li class="footer-li"><a class="footer-a" href="https://plus.google.com/115943371652466786884/about" target="_blank"><i class="icon fa fa-google-plus-square fa-lg"></i></a></li>
                         <li class="footer-li"><a class="footer-a" href="https://www.facebook.com/stiwou" target="_blank"><i class="icon fa fa-facebook-square fa-lg"></i></a></li>
                         <li class="footer-li"><a class="footer-a" href="https://www.youtube.com/user/Stiwoz/feed" target="_blank"><i class="icon fa fa-youtube-square fa-lg"></i></a></li>
                         <li class="footer-li"><a class="footer-a" href="http://stefanosanvito.altervista.org/" target="_blank"><i class="icon fa fa-globe fa-lg"></i></a></li>
                     </ul>
-                </td>
-                <td><div class="spock"><i class="fa fa-hand-spock-o fa-fw"></i></div></td>
-            </tr>
-        </table>
-    </div>
-    
+                </div>
+                <div class="col-md-1">
+                    <a class="footer-a" id="flag" href="eng/index.html"><img src="img/engIconSmall.png"/></a>
+                </div>
+            </div>
+        </div>
+    </nav>
+
     <!--- --- --- Scripts & Plug-Ins --- --- --->
     <script src="../js/jquery-1.11.3.min.js"></script>
     <script src="../bootstrap-3.3.6-dist/js/bootstrap.min.js"></script>
